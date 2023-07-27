73e1eb7427dadd1aacb8c9ea719b7208673b91b3
Ircaro <ircaro.seixas@zig.fun>
2023-07-27 18:16:03 -0300

    Salvando

--- Estudos_CSharp/3. Operações de Comparação/DiferenteDe.cs
+++ Estudos_CSharp/3. Operações de Comparação/DiferenteDe.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Estudos_CSharp._3._Operações_de_Comparação
 {
     internal class DiferenteDe
     {
+
     }
 }
