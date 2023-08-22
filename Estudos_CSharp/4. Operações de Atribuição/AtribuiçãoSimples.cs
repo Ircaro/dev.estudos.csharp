d23f8830c0cae165df63d020d58bcfcf2f95f587
Írcaro Seixas <ircaro.th@gmail.com>
2023-08-21 22:56:45 -0300

    Começando com Operações de Atribuição.

---
+++ Estudos_CSharp/4. Operações de Atribuição/AtribuiçãoSimples.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Estudos_CSharp._4._Operações_de_Atribuição
+{
+    internal class AtribuiçãoSimples
+    {
+        public static void executar()
+        {
+            Console.WriteLine(" --- OPERAÇÕES DE ATRIBUIÇÃO: ATRIBUIÇÃO SIMPLES --- \n\n");
+
+            Console.WriteLine("A atribuição simples ( = ) é utilizada para armazenar um valor em uma variável\n\n");
+
+            Console.WriteLine("Para criar e atribuir um valor a uma variárvel é necessário informar o tipo da variável, nome da variável e o valor que está se atribuindo a ela, necessariamente nessa ordem.\n\n");
+
+            Console.WriteLine("Exemplo: int idade = 20\n\n");
+
+            Console.WriteLine("No exemplo, estamos declarando uma variável com o tipo int (inteiro) com o nome idade e atribuindo o valor de 20 para ela.\n\n");
+
+            int idade = 20;
+
+            Console.WriteLine("No momento, o valor da nossa variável idade está com o valor: " + idade + "\n\n");
+
+            Console.WriteLine("Mas é possível alterar o valor que contém na variável, neste caso, não precisamos mais informar o tipo, pois ele já foi declarado, ficando somente: idade = 30, por exemplo: \n\n");
+
+            idade = 30;
+
+            Console.WriteLine("E agora o valor da variável está igual a: " + idade + "\n\n");
+
+            Console.WriteLine("Existem outros tipos de variáveis além do int, como double, string, bool, etc.\n\n");
+
+            Console.WriteLine("------------------------------------------------- \n");
+
+        }
+    }
+}
--- Estudos_CSharp/Program.cs
+++ Estudos_CSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Estudos_CSharp._2._Operações_Lógicas;
 using Estudos_CSharp._3._Operações_de_Comparação;
+using Estudos_CSharp._4._Operações_de_Atribuição;
 using Estudos_CSharp.Operações;
 using System;
 
@@ -8,20 +9,26 @@ class Atividade
     static void Main(string[] args)
     {
         // Operações Matemáticas
-        //Soma.executar();
-        //Subtração.executar();
-        //Multiplicação.executar();
-        //Divisão.executar();
+        // Soma.executar();
+        // Subtração.executar();
+        // Multiplicação.executar();
+        // Divisão.executar();
 
-        //// Operações Lógicas
-        //AND.executar();
-        //OR.executar();
-        //NOT.executar();
+        // Operações Lógicas
+        // AND.executar();
+        // OR.executar();
+        // NOT.executar();
 
         // Operações de Comparação
-        Igual.executar();
-        MaiorQue.executar();
-        MenorQue.executar();
+        // Igual.executar();
+        // MaiorQue.executar();
+        // MenorQue.executar();
+        // MaiorOuIgual.executar();
+        // MenorOuIgual.executar();
+
+        // Operações de Atribuição
+        AtribuiçãoSimples.executar();
+
     }
 
 }
